1d7ff5270dafd986cad641e95fc9aacab92f2ec4
MaverickEdwards <r.edwards0917@gmail.com>
2022-11-02 01:28:21 -0400

    commit tree 7d06f8efd1d6650a01a7cd2d615567dae4df31a5

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if (someNumber <= 400) {
+    return 'This one is on me!'
+  }
+  else if (someNumber > 400 && someNumber < 2000){
+  return 'That will be twenty bucks.'
+  }
+  else if (someNumber > 2000 && someNumber < 2500){
+  return 'I will gladly take your thirty bucks.'
+  }
+  else if (someNumber > 2500){
+    return 'No can do.';
+}
+}
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.': 'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+switch (tip) {
+  case 'generous': return 'Thank you so much.'
+  
+  case 'not as generous' : return 'Thank you.'
+  
+  default : return 'Bye.'
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
